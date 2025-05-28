e2030f7aea5d5bfb2fda494ebd62499ffb2c002c
Fernando Rojas <frojasq@yahoo.com>
2025-05-28 18:24:31 -0500

    [zifherx] alistado observaciones

--- components/shared/CounterItem.tsx
+++ components/shared/CounterItem.tsx
@@ -1,20 +1,27 @@
-"use client"
+"use client";
 
-import CountUp from 'react-countup'
+import CountUp from "react-countup";
 
-import { CounterProp } from "@/types/Props"
+import { CounterProp } from "@/types/Props";
 
-export function CounterItem({end, label, lineRight,lineRightMobile}: CounterProp) {
-  
-    return (
+export function CounterItem({
+  end,
+  label,
+  lineRight,
+  lineRightMobile,
+}: CounterProp) {
+  return (
     <div className={`${lineRight && "ltr"}`}>
-        <div className={`${lineRight && "px-10 border-2 border-transparent md:border-e-white"} ${lineRightMobile && "border-e-white"}`}>
-          <p className="flex mb-2 text-2xl font-extrabold md:text-4xl text-white">
-            +{" "}
-            <CountUp end={end} start={0} duration={2.5} startOnMount/>
-          </p>
-          <p className="uppercase max-w-[100px]">{label}</p>
-        </div>
+      <div
+        className={`${
+          lineRight && "px-10 border-2 border-transparent md:border-e-white"
+        } ${lineRightMobile && "border-e-white"}`}
+      >
+        <p className="flex mb-2 text-2xl font-extrabold md:text-4xl text-white">
+          + <CountUp end={end} start={0} duration={2.5} startOnMount />
+        </p>
+        <p className="uppercase max-w-[100px]">{label}</p>
+      </div>
     </div>
-  )
+  );
 }
--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -12,7 +12,7 @@ export function Footer() {
   const today = new Date().getFullYear();
   return (
     <footer className="bg-blueAmbiente text-white">
-      <div className="container mx-auto px-4 py-12 md:py-16">
+      <div className="max-w-7xl mx-auto px-4 py-12 md:py-16">
         <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-4">
           <div className="space-y-4">
             <Image
